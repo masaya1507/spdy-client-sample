10a84e7aeb68f330e7291d641705c0415a200cc5
unknown <partner_m@ISJPJSIO1715969.vfjp-sales>
2013-12-27 17:19:56 +0900

    added resouces

---
+++ src/test/java/jp/projects/miya/spdy_client_sample/AppTest.java
@@ -0,0 +1,15 @@
+package jp.projects.miya.spdy_client_sample;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void testApp() throws Exception {
+		App.main(new String[] {
+
+		});
+	}
+}
